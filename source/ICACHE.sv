724e8181ec8ac0792c218db25bcf33e1fafd5867
chadcoons <coons@purdue.edu>
2014-10-20 15:48:15 -0400

    starting icache

---
+++ source/ICACHE.sv
@@ -0,0 +1,9 @@
+//`include "cache_control_if.vh"
+//`include "datapath_cache_if.vh"
+
+module ICACHE(
+	      input cache_control_if.cc ccif,
+	      input datapath_cache_if.dc dcif
+	      );
+
+   
