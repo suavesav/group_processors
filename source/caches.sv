4aa5da6b8b6772dccba1cb14981bd47e24317693
chadcoons <coons@purdue.edu>
2014-10-27 14:30:38 -0400

    caches send to ICACHE and DCACH

--- source/caches.sv
+++ source/caches.sv
@@ -14,25 +14,25 @@ module caches (
   parameter CPUID = 0;
 
   // icache
-  icache  ICACHE(dcif, ccif);
+  icache  ICACHE(CLK, nRST, dcif, ccif);
   // dcache
-  //dcache  DCACHE(dcif, ccif);
+  dcache  DCACHE(CLK, nRST, dcif, ccif);
 
   // dcache invalidate before halt handled by dcache when exists
-  assign dcif.flushed = dcif.halt;
+  //assign dcif.flushed = dcif.halt;
 
   //singlecycle
   //assign dcif.ihit = (dcif.imemREN) ? ~ccif.iwait[CPUID] : 0;
-  assign dcif.dhit = (dcif.dmemREN|dcif.dmemWEN) ? ~ccif.dwait[CPUID] : 0;
+  //assign dcif.dhit = (dcif.dmemREN|dcif.dmemWEN) ? ~ccif.dwait[CPUID] : 0;
   //assign dcif.imemload = ccif.iload[CPUID];
-  assign dcif.dmemload = ccif.dload[CPUID];
+  //assign dcif.dmemload = ccif.dload[CPUID];
 
 
   //assign ccif.iREN[CPUID] = dcif.imemREN;
-  assign ccif.dREN[CPUID] = dcif.dmemREN;
-  assign ccif.dWEN[CPUID] = dcif.dmemWEN;
-  assign ccif.dstore[CPUID] = dcif.dmemstore;
+  //assign ccif.dREN[CPUID] = dcif.dmemREN;
+  //assign ccif.dWEN[CPUID] = dcif.dmemWEN;
+  //assign ccif.dstore[CPUID] = dcif.dmemstore;
   //assign ccif.iaddr[CPUID] = dcif.imemaddr;
-  assign ccif.daddr[CPUID] = dcif.dmemaddr;
+  //assign ccif.daddr[CPUID] = dcif.dmemaddr;
 
 endmodule
